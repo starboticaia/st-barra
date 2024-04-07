b80b10de7e574479afd0936e50df02e31e04afcb
starboticaia <165588679+starboticaia@users.noreply.github.com>
2024-04-07 20:35:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write("Hola mundo")
+edad = st.slider("Dime tu edad",max_value=130)
+
+if edad<16:
+  st.write("Es obligatorio que asistas a clase.")
+else:
+  if edad<67:
+    st.write("Estás en edad de trabajar")
+  else:
+    st.write("A disfrutar de la jubilación")
